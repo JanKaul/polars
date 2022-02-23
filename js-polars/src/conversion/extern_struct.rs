29a88d5aeb8be57f7c28f16bef70cd856bab2f9b
Jan Kaul <jankaul@mailbox.org>
2022-02-23 13:21:33 +0100

    add extern_struct macro

---
+++ js-polars/src/conversion/extern_struct.rs
@@ -0,0 +1,34 @@
+use wasm_bindgen::convert::{FromWasmAbi, RefFromWasmAbi};
+
+pub trait IntoRustStruct<T: FromWasmAbi<Abi = u32>> {
+    fn to_rust(self) -> T;
+}
+
+pub trait RefRustStruct<'a, T: RefFromWasmAbi<Abi = u32>> {
+    fn to_rust(self) -> wasm_bindgen::__rt::Ref<'static, T>;
+}
+
+#[macro_export]
+macro_rules! extern_struct {
+    ( $x:ident, $y:ty) => {
+        paste! {
+        #[wasm_bindgen]
+        extern "C" {
+            #[wasm_bindgen(getter = ptr)]
+            fn [<$x _ptr>](this: &$x) -> f64;
+        }
+
+        impl<'a> IntoRustStruct<$y> for $x {
+            fn to_rust(self) -> $y {
+                unsafe { $y::from_abi([<$x _ptr>](&self) as u32) }
+            }
+        }
+
+        impl<'a> RefRustStruct<'a, $y> for &'a $x {
+            fn to_rust(self) -> wasm_bindgen::__rt::Ref<'static, $y> {
+                unsafe { $y::ref_from_abi([<$x _ptr>](self) as u32) }
+            }
+        }
+        }
+    };
+}
--- js-polars/src/conversion/mod.rs
+++ js-polars/src/conversion/mod.rs
@@ -1,2 +1,5 @@
 pub mod array;
+pub mod extern_struct;
+pub mod one_or_more;
 pub mod typed_array;
+pub mod value_of;
